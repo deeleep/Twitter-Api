0b2446c987bd56903d9457d3a97bd00c0285e80c
Dilip Mehata <deeleepkushwaha@gmail.com>
2022-06-15 14:30:00 +0900

    Initial commit

---
+++ src/main/java/com/dlpmeh/ApiTwitterApplication.java
@@ -0,0 +1,13 @@
+package com.dlpmeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTwitterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTwitterApplication.class, args);
+	}
+
+}
